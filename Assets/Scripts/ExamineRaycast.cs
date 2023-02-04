4b7ba87ecc865cc484dab5a70fe0eea1d1d64329
BryleBanatin <104262539+BryleBanatin@users.noreply.github.com>
2023-02-04 10:02:36 +0800

    v0.1

--- Assets/Scripts/ExamineRaycast.cs
+++ Assets/Scripts/ExamineRaycast.cs
@@ -6,33 +6,81 @@ using UnityEngine.UI;
 
 public class ExamineRaycast : MonoBehaviour
 {
+    public bool isExamining;
+    private string lastItem;
+
     RaycastHit hit;
     [SerializeField] Camera PlayerCam;
     [SerializeField] GameObject UI;
+    [SerializeField] GameObject Exit;
+
+    private void Start()
+    {
+        Exit.gameObject.SetActive(false);
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            if(Physics.Raycast(PlayerCam.transform.position, Camera.main.transform.forward, out hit, 100.0f ))
+            ExamineItem();
+        }
+        LockCursorFunc();
+    }
+
+    private void ExamineItem()
+    {
+        if (Physics.Raycast(PlayerCam.transform.position, Camera.main.transform.forward, out hit, 100.0f))
+        {
+            if (hit.collider.tag == "Examinable")
             {
-                if (hit.collider.tag == "Examinable")
+                Debug.Log(hit.collider.name + "Image");
+                if (UI.transform.Find(hit.collider.name + "Image"))
                 {
-                    Debug.Log(hit.collider.name + "Image");
-                    if (UI.transform.Find(hit.collider.name + "Image"))
+                    GameObject ObjSprite = GameObject.Find(hit.collider.name + "Image");
+                    lastItem = hit.collider.name + "Image";
+                    Image[] ObjImage = ObjSprite.GetComponentsInChildren<Image>();
+                    Debug.Log(ObjImage.Length);
+                    foreach (Image i in ObjImage)
                     {
-                        GameObject ObjSprite = GameObject.Find(hit.collider.name + "Image");
-                        Image[] ObjImage = ObjSprite.GetComponentsInChildren<Image>();
-                        Debug.Log(ObjImage.Length);
-                        foreach(Image i in ObjImage)
-                        {
-                            i.enabled = true;
-                        }    
+                        if (i.gameObject.tag != "ItemBtn")
+                        i.enabled = true;
                     }
+                    Exit.gameObject.SetActive(true);
+                    isExamining = true;
                 }
             }
         }
+    }
 
+    public void HideItem()
+    {
+        GameObject ObjSprite = GameObject.Find(lastItem);
+        Image ObjImage = ObjSprite.GetComponentInChildren<Image>();
+        ObjImage.enabled = false;
+
+    }
+    private void LockCursorFunc()
+    {
+        FirstPersonController fpsController = this.gameObject.GetComponent<FirstPersonController>();
+        if (isExamining == true)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            fpsController.playerCanMove = false;
+            fpsController.lockCursor = false;
+            fpsController.cameraCanMove = false;
+            fpsController.enableHeadBob = false;
+            fpsController.enableZoom = false;
+        }
+        else if (isExamining == false)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            fpsController.playerCanMove = true;
+            fpsController.lockCursor = true;
+            fpsController.cameraCanMove = true;
+            fpsController.enableHeadBob = true;
+            fpsController.enableZoom = true;
+        }
     }
 }
